0731c74b6ee14610036e72e5ef42638d7132a332
xjy123456wwx <3098632509@qq.com>
2026-06-23 18:18:21 +0800

    6.23

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 const gravity = 2000
-const speed_walk = 300
-const speed_run = 550
-const jump_force = 1600
+const speed_walk = 800
+const speed_run = 1550
+const jump_force = 1800
 
 var max_jumps = 2
 var jumps_remaining = max_jumps
@@ -15,6 +15,11 @@ var jumps_remaining = max_jumps
 
 var jump_trigger = false
 var is_buzhuo = false
+
+var health = 100
+var can_hurt = true
+
+var is_in_emeny : bool = false
 
 func _ready():
 	# 开局直接把捕捉动画隐藏，避免叠在身上
@@ -50,7 +55,7 @@ func _physics_process(delta):
 		run_anim.scale.x = -1 if direction < 0 else 1
 		buzhuo_anim.scale.x = -1 if direction < 0 else 1
 
-	# 只在地面、不在捕捉状态时，按右键触发
+	# 只在地面、不在捕捉状态时，按右键触发捕捉
 	if Input.is_action_just_pressed("buzhuo") and not is_buzhuo and is_on_floor():
 		is_buzhuo = true
 		velocity.x = 0
@@ -96,3 +101,53 @@ func _physics_process(delta):
 func _process(_delta):
 	if Input.is_action_just_pressed("jump") and jumps_remaining > 0 and not is_buzhuo:
 		jump_trigger = true
+	if can_hurt and is_in_emeny:
+		health -= 10
+		print("【强制受伤】已扣血")
+		hurt_flash()
+		can_hurt = false
+		await get_tree().create_timer(1.0).timeout
+		can_hurt = true
+
+
+# 角色受伤闪红
+
+func _on_hurt_box_area_exited(area: Area2D) -> void:
+	is_in_emeny = true
+	
+	print("body name: ", area.name)
+	if can_hurt:
+			health -= 10
+			print("【强制受伤】已扣血")
+			hurt_flash()
+			can_hurt = false
+			await get_tree().create_timer(1.0).timeout
+			can_hurt = true
+
+
+#func _on_hurt_box_area_entered(area: Area2D) -> void:
+	#pass # Replace with function body.
+
+
+func _on_hurt_box_body_exited(_body: Node2D) -> void:
+	print("exited")
+	is_in_emeny = true
+
+
+func hurt(demage:int)->void:
+	if not can_hurt:
+		return
+	health -= demage
+	# 红光
+	
+func hurt_flash():
+	print("闪红函数执行")
+	var sprites = [walk_anim, jump_anim, run_anim, buzhuo_anim]
+	# 一次性全部变红
+	for s in sprites:
+		s.modulate = Color.RED
+	# 统一等待0.2秒
+	await get_tree().create_timer(0.2).timeout
+	# 一次性全部恢复原色
+	for s in sprites:
+		s.modulate = Color.WHITE
